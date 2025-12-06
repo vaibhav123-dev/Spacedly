479d0cb90975452fa9fad18b04566aa8b6458219
Vaibhav Darvekar <vabdarwekar00@gmail.com>
2025-12-06 22:33:35 +0530

    update

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,12 +1,18 @@
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+const config = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  dialect: process.env.DB_DIALECT || 'mysql',
+};
+
 module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  },
+  development: config,
+  production: config,
+  test: config,
 };
